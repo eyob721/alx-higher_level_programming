0330e4a28ca17254e900aeaa205bfd28375a95e9
eyob721 <eyobmelkamu721@gmail.com>
2023-09-11 13:02:48 +0300

    Fix 11-delete_at.py: Adjust condition for index, include 0

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,6 +11,6 @@ def delete_at(my_list=[], idx=0):
         - If idx is negative or out of range, nothing is changed.
 
     """
-    if my_list is not None and 0 < idx < len(my_list):
+    if my_list is not None and 0 <= idx < len(my_list):
         del my_list[idx]
     return my_list
