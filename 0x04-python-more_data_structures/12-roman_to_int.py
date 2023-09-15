3b205e92d3674c6ac5a247064d0696e302fd8cee
eyob721 <eyobmelkamu721@gmail.com>
2023-09-15 12:48:37 +0300

    Fix 12-roman_to_int.py: handle subtractive forms

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,16 +10,24 @@ def roman_to_int(roman_string):
         - converted integer
         - If the `roman_string` is not a string or None, it returns 0
 
+    Description:
+        - In the cases of subtractive form, which are the cases of IV, IX, XC,
+          XL, CD, or CM. In this cases the value of the first roman numeral is
+          subtracted from the value of the second numeral.
+          (e.g. IX = 10 - 1 = 9)
+
     """
-    integer = 0
+    roman_integer = 0
     conversion = \
         {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) is str:
-        prev = ''
+        prev_int = -1  # -1, a non roman numeral number
         for ch in roman_string:
-            roman_int = conversion.get(ch, 0)
-            if ch != 'I' and prev == 'I':
-                roman_int -= 2  # -2 beacause 'I' is already added
-            prev = ch
-            integer += roman_int
-    return integer
+            cur_int = conversion.get(ch, 0)
+            in_subtractive_form = \
+                cur_int // prev_int == 5 or cur_int // prev_int == 10
+            if in_subtractive_form:
+                cur_int -= prev_int * 2  # 2 because prev_int is added already
+            prev_int = cur_int
+            roman_integer += cur_int
+    return roman_integer
