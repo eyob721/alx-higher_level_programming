c7b54b9d06ea04652b34b35c02aea22437cdb172
eyob721 <eyobmelkamu721@gmail.com>
2023-09-27 16:08:56 +0300

    Finish task 0: 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    A function that prints `x` elements from `my_list`
+
+    Args:
+        my_list: a given list
+        x: number of elements to print
+
+    Returns:
+        - number of items printed
+
+    """
+    printed = i = 0
+    try:
+        while i < x:
+            print("{}".format(my_list[i]), end="")
+            printed = i = i + 1
+        print()
+    except Exception:
+        pass
+    return printed
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -1,1 +1,10 @@
 # 0x05. Python - Exceptions
+
+## Mandatory
+
+[0-safe_print_list.py](./0-safe_print_list.py)
+
+- A function that prints `x` elements from a list.
+- The list can contain items of any type.
+- `x` can be bigger than the length of the list.
+- `try: / except:` clauses must be used.
