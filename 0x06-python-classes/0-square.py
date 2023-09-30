464108f437c7297527a8a13060e6d44d6274c57e
eyob721 <eyobmelkamu721@gmail.com>
2023-09-30 13:37:58 +0300

    Finish task 0: 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module for Task 0.
+
+Description:
+    In this module we write an empty class for a Square object.
+
+"""
+
+
+class Square:
+    """An empty class that defines a Square object.
+
+    """
+    pass
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,1 +1,7 @@
 # 0x06. Python - Classes and Objects
+
+## Mandatory
+
+[0-square.py](./0-square.py)
+
+- A module that contains an empty class that defines a Square object.
