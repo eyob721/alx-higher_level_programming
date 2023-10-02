787f1661e524123c05ef0abd58749169fb6ed0b0
eyob721 <eyobmelkamu721@gmail.com>
2023-10-02 16:48:54 +0300

    Feat task 10 101-nqueens.py: save work on the n queens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that solves the `N queens` problem."""
+
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    exit(1)
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
