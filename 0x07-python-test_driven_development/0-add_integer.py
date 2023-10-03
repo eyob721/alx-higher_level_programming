5f7310c1403e93e923ddf91343e7a2afde3ff94b
eyob721 <eyobmelkamu721@gmail.com>
2023-10-03 14:03:55 +0300

    Finish task 0: 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+"""A module for Task 0."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Note:
+        The values `a` and `b` are first casted into integers.
+
+    Args:
+        a (int | float): First integer
+        b (int | float): Second integer
+
+    Returns:
+        Sum of `a` and `b`.
+
+    Raises:
+        TypeError: If either `a` or `b` are not int or float.
+
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,1 +1,12 @@
 # 0x07. Python - Test-driven development
+
+In this project all the test files for each task are provided in the `tests`
+directory. The functions in each task are executed with this command:
+`python3 -m doctest ./tests/<testfile>`
+
+## Mandatory
+
+[0-add_integer.py](./0-add_integer.py)<br>
+`testfile:` [tests/0-add_integer.txt](./tests/0-add_integer.txt)
+
+- A function that adds 2 integers.
