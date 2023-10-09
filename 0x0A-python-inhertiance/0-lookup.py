0cb64c040d6759a62f3e68ad8833eda02396f13a
eyob721 <eyobmelkamu721@gmail.com>
2023-10-09 19:33:31 +0300

    Finish task 0: 0-lookup.py

---
+++ 0x0A-python-inhertiance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module for task 1."""
+
+
+def lookup(obj):
+    """Returns list of available attributes and methods of an object."""
+    return dir(obj)
--- 0x0A-python-inhertiance/README.md
+++ 0x0A-python-inhertiance/README.md
@@ -1,1 +1,8 @@
 # 0x0A. Python - Inheritance
+
+## Mandatory
+
+[0-lookup.py](./0-lookup.py)
+
+- A function that returns list of attributes and methods of an object.
+
