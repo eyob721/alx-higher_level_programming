a7ad40bd64ec7427c9a435410127d4938d7b2dd1
eyob721 <eyobmelkamu721@gmail.com>
2023-10-10 04:34:09 +0300

    Fix 10-square.py: Validate size with name size, not width or height

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,8 +7,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Class definition of a Square."""
     def __init__(self, size):
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """Area of Square"""
