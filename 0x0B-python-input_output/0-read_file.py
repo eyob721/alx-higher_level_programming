c737b4ba7725e321ee9a17d051cc21e3825ca21d
eyob721 <eyobmelkamu721@gmail.com>
2023-10-11 04:22:40 +0300

    Finish task 0: 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module for task 0."""
+
+
+def read_file(filename=""):
+    """Reads a file."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print("{}".format(file.read()))
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,2 +1,7 @@
 # 0x0B. Python - Input/Output
 
+## Mandatory
+
+[0-read_file.py](./0-read_file.py)
+
+- A function that reads a file in `utf-8` encoding and prints it to stdout.
