2610870450aca4a0567184c00bde39aaa63f735f
eyob721 <eyobmelkamu721@gmail.com>
2023-10-13 12:50:31 +0300

    Fix 103-magic_class.py (0x06): Add comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""A module for task 10."""
 import math
 
 
 class MagicClass:
+    """Class definition derived from given Bytecode."""
     def __init__(self, radius):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -10,8 +12,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-
+        """Area of the MagicClass :)"""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
+        """Circumference of the MagicClass :)"""
         return 2 * math.pi * self.__radius
