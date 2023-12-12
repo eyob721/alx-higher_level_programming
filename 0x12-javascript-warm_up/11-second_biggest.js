205cbbff59214e0423cab7d1a87a2de1eddc0986
eyob721 <eyobmelkamu721@gmail.com>
2023-12-12 05:57:36 +0300

    feat: finish task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+
+function toNumberArray (arr) {
+  const numArray = [];
+  for (const i in arr) numArray.push(parseInt(arr[i]));
+  return numArray;
+}
+
+if (argv.length <= 2) console.log(0);
+else {
+  console.log(
+    toNumberArray(argv).sort(function (a, b) {
+      return b - a;
+    })[1]
+  );
+}
