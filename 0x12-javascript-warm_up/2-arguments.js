9dbce132d8292aea3b888a3ee9ca5b3837b71011
eyob721 <eyobmelkamu721@gmail.com>
2023-12-12 04:51:47 +0300

    fix: use process instead of node:process

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const argc = argv.slice(2).length;
-if (argc === 0) console.log('No argument');
-else if (argc === 1) console.log('Argument found');
+const argv = process.argv.slice(2);
+if (argv.length === 0) console.log('No argument');
+else if (argv.length === 1) console.log('Argument found');
 else console.log('Arguments found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const argv0 = require('node:process').argv.slice(2)[0];
+const argv0 = process.argv.slice(2)[0];
 if (!argv0) console.log('No argument');
 else console.log(argv0);
