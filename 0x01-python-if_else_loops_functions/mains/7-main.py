ece81866bf159a46c3a8fc4e629161680420153b
eyob721 <eyobmelkamu721@gmail.com>
2024-01-16 13:04:32 +0300

    feat: finish task 7

---
+++ 0x01-python-if_else_loops_functions/mains/7-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+islower = __import__("7-islower").islower
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
