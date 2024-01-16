e92c16a6456db169868ba200c9e09824235f819d
eyob721 <eyobmelkamu721@gmail.com>
2024-01-16 13:44:13 +0300

    feat: finish task 15

---
+++ 0x01-python-if_else_loops_functions/mains/101-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+remove_char_at = __import__("101-remove_char_at").remove_char_at
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
