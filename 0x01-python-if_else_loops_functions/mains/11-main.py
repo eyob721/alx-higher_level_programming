3cf16d39f7e06b0ada5a9e7fad300290a4d6567c
eyob721 <eyobmelkamu721@gmail.com>
2024-01-16 13:20:16 +0300

    feat: finish task 11

---
+++ 0x01-python-if_else_loops_functions/mains/11-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+pow = __import__("11-pow").pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
