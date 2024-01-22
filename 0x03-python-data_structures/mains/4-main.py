20814e954d7ac77209be53984439831e392496d8
eyob721 <eyobmelkamu721@gmail.com>
2024-01-22 13:18:14 +0300

    feat: finish task 4

---
+++ 0x03-python-data_structures/mains/4-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+new_in_list = __import__("4-new_in_list").new_in_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
