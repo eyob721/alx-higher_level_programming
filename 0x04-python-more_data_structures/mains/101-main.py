b78a816c756ff8306d6d06af0d9a313beeab05cc
eyob721 <eyobmelkamu721@gmail.com>
2024-01-25 12:01:50 +0300

    feat: finish task 14

--- 0x04-python-more_data_structures/mains/101-main.py
+++ 0x04-python-more_data_structures/mains/101-main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-square_matrix_map = \
-    __import__('101-square_matrix_map').square_matrix_map
+import os
+import sys
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+square_matrix_map = __import__("101-square_matrix_map").square_matrix_map
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 new_matrix = square_matrix_map(matrix)
 print(new_matrix)
