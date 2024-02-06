cf42d473079b1445e608567e39fd4da6b6135d02
eyob721 <eyobmelkamu721@gmail.com>
2024-02-06 18:56:07 +0300

    feat: finish task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 1"""
+
+
+class Square:
+    """A class that defines a Square object"""
+
+    def __init__(self, size):
+        """Square constructor
+
+        Args:
+            size: Initial size of the square object
+
+        """
+        self.__size = size
