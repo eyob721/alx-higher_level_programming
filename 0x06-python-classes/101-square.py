be3ba8a84b5a17cd9a7169e699e877d872a88ab5
eyob721 <eyobmelkamu721@gmail.com>
2024-02-06 19:42:08 +0300

    feat: finish task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Task 8"""
+
+
+class Square:
+    """A class that defines a Square object"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Square constructor
+
+        Args:
+            size: Initial size of the square object
+                  It must be an integer and >= 0
+            position: Initial position of the square object
+                      It must be a tuple of 2 integers
+
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Size of the square object"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Position of the square object"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        for i in value:
+            if type(i) is not int or i < 0:
+                raise TypeError(
+                    "position must be a tuple of 2 positive integers"
+                )
+        self.__position = value
+
+    def area(self):
+        """Area of the square object.
+
+        Returns:
+            int: Area of the square object.
+
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square using the # character, in the current position"""
+        print(self.__str__())
+
+    def __str__(self):
+        """String representation of the Square object"""
+        x = self.position[0]
+        y = self.position[1]
+        sqr = "\n" * y + (" " * x + "#" * self.__size + "\n") * self.__size
+        return sqr.rstrip("\n")
---
+++ 0x06-python-classes/mains/101-main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Task 8 - test script"""
+
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+Square = __import__("101-square").Square
+
+my_square = Square(0, (0, 0))
+print(my_square)
+
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
