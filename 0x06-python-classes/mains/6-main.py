805f9380d4cf6313d70110115f0f91d6b6dda952
eyob721 <eyobmelkamu721@gmail.com>
2024-02-06 19:27:08 +0300

    feat: finish task 6

---
+++ 0x06-python-classes/mains/6-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Task 6 - test script"""
+
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+Square = __import__("6-square").Square
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
