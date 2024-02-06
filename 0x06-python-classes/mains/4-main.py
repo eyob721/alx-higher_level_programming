0711dfc619afce85ed1973c52c326b9f18f0c138
eyob721 <eyobmelkamu721@gmail.com>
2024-02-06 19:07:56 +0300

    feat: finish task 4

---
+++ 0x06-python-classes/mains/4-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Task 4 - test script"""
+
+import os
+import sys
+
+# Add module from parent directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+Square = __import__("4-square").Square
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
