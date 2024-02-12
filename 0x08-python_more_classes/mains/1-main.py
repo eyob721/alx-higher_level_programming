0e190aae98f7f468cf7685eb6d34e6c45371117f
eyob721 <eyobmelkamu721@gmail.com>
2024-02-12 16:11:45 +0300

    feat: finish task 1

---
+++ 0x08-python_more_classes/mains/1-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Main script for Task 1"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+Rectangle = __import__("1-rectangle").Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
