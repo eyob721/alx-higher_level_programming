1e983ba00644ca7f62a185497a7fa3ab9a2f1080
eyob721 <eyobmelkamu721@gmail.com>
2024-02-21 16:52:10 +0300

    docs: add additional answer

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,5 @@
 
 def is_kind_of_class(obj, a_class):
     """Returns True if an object is a subclass of a class."""
+    # Also works if you use isinstance()
     return issubclass(type(obj), a_class)
