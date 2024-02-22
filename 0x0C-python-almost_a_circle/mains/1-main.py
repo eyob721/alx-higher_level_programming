e71c44e99e26352b09d464f422ec55f3ac2c5eec
eyob721 <eyobmelkamu721@gmail.com>
2024-02-22 22:59:48 +0300

    feat: finish task 1

---
+++ 0x0C-python-almost_a_circle/mains/1-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Task 1"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.base import Base  # noqa
+
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
