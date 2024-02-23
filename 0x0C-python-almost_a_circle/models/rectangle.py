3e07989900d43a00e644af2c2871a8c3c9bdd1ba
eyob721 <eyobmelkamu721@gmail.com>
2024-02-23 10:44:36 +0300

    feat: add area attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """Area of the Rectangle"""
+        return self.__width * self.__height
