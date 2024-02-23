7eda1f523081f96200ed883dc094b51ccdd6e054
eyob721 <eyobmelkamu721@gmail.com>
2024-02-23 10:33:00 +0300

    feat: finish task 3

---
+++ 0x0C-python-almost_a_circle/mains/3-main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Task 3"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.rectangle import Rectangle  # noqa
+
+if __name__ == "__main__":
+
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
