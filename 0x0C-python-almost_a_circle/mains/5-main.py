43d33a78f80b975554a4061d50200c805f50f03d
eyob721 <eyobmelkamu721@gmail.com>
2024-02-23 21:44:56 +0300

    feat: finish task 5

---
+++ 0x0C-python-almost_a_circle/mains/5-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 5"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.rectangle import Rectangle  # noqa
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6)
+    r1.display()
+
+    print("---")
+
+    r1 = Rectangle(2, 2)
+    r1.display()
