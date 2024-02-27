a6ea0e6ecc4b78ecfb0f8bb7295c09011b2fff4f
eyob721 <eyobmelkamu721@gmail.com>
2024-02-27 22:50:22 +0300

    feat: finish task 10

---
+++ 0x0C-python-almost_a_circle/mains/10-main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Task 10"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.square import Square  # noqa
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
+
+    print("---")
+
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
+
+    print("---")
+
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
