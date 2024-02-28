0b481efbc2fa0760419ce18e763abfa372c7f220
eyob721 <eyobmelkamu721@gmail.com>
2024-02-28 12:04:13 +0300

    feat: finish task 16

---
+++ 0x0C-python-almost_a_circle/mains/16-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 16"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.rectangle import Rectangle  # noqa
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    Rectangle.save_to_file([r1, r2])
+
+    with open("Rectangle.json", "r") as file:
+        print(file.read())
