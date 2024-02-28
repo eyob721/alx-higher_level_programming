8563679ef106c6c202f123c7c2ad77e3cbf7348d
eyob721 <eyobmelkamu721@gmail.com>
2024-02-28 12:02:39 +0300

    feat: finish task 15

---
+++ 0x0C-python-almost_a_circle/mains/15-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 15"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.base import Base  # noqa
+from models.rectangle import Rectangle  # noqa
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
