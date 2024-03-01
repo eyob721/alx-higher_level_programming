42e128b8b0017689358db8120bedfd783eabd300
eyob721 <eyobmelkamu721@gmail.com>
2024-03-01 09:02:02 +0300

    feat: finish task 18

---
+++ 0x0C-python-almost_a_circle/mains/18-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 18"""
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models.rectangle import Rectangle  # noqa
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(3, 5, 1)
+    r1_dictionary = r1.to_dictionary()
+    r2 = Rectangle.create(**r1_dictionary)
+    print(r1)
+    print(r2)
+    print(r1 is r2)
+    print(r1 == r2)
