62934e211eaedc46857b7ff0266f2fad1a1d72d7
eyob721 <eyobmelkamu721@gmail.com>
2024-03-26 22:58:43 +0300

    feat: finish task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,12 @@
+-- Lists all the records of the table `second_table` of the database `hbtn_0c_0`
+-- It doesn't include rows without a name value, and the list is sorted
+-- according to the score in descending order
+SELECT
+  score,
+  name
+FROM
+  second_table
+WHERE
+  name IS NOT NULL
+ORDER BY
+  score DESC;
