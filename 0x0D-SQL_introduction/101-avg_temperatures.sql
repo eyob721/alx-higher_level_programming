dc4e0cb17d1ac4a20798891b784ca8aad7650dad
eyob721 <eyobmelkamu721@gmail.com>
2024-03-26 23:22:03 +0300

    feat: finish task 18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,13 @@
+-- Displays the average temperature by city ordered by temperature (descending)
+USE hbtn_0c_0;
+
+
+SELECT
+  city,
+  AVG(value) AS avg_temp
+FROM
+  temperatures
+GROUP BY
+  city
+ORDER BY
+  avg_temp DESC;
