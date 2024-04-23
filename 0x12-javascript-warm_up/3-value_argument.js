c10fdae188cacebcf4f2df2a626918d7fc9645dc
eyob721 <eyobmelkamu721@gmail.com>
2024-04-23 09:01:49 +0300

    fix: do not use length, check with undefined instead

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /* NOTE: To get the actual arguments to the script start from the 2nd index */
 const argv = require('node:process').argv.slice(2);
-
-if (argv.length === 0) {
+if (argv[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(argv[0]);
