7bae21b0ab6b43df14f5b274fa14a6bd24767fc7
eyob721 <eyobmelkamu721@gmail.com>
2024-04-23 13:06:57 +0300

    feat: finish task 12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/* DO NOT CHANGE THE CODE ABOVE */
+myObject.value = 89;
+/* DO NOT CHANGE THE CODE BELOW */
+console.log(myObject);
