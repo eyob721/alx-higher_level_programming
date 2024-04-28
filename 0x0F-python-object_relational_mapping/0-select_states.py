e384377289f9eb4ba19328d09a97474041ea34e0
eyob721 <eyobmelkamu721@gmail.com>
2024-04-28 21:58:16 +0300

    feat: finish task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Task 0 - MySQLdb
+
+Description:
+    The mysql username, mysql password and mysql database name must be
+    passed as argumnets to the script
+    Usage: ./0-select_states.py username password database
+
+"""
+from sys import argv
+
+import MySQLdb
+
+if __name__ == "__main__":
+
+    connection_arguments = {
+        "host": "localhost",
+        "port": 3306,
+        "user": argv[1],  # mysql username
+        "password": argv[2],  # mysql password
+        "database": argv[3],  # mysql database name
+    }
+
+    with MySQLdb.connect(**connection_arguments) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id;")
+        record = cursor.fetchall()
+        for rec in record:
+            print(rec)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,1 +1,15 @@
 # 0x0F. Python - Object-relational mapping
+
+## Mandatory
+
+[0-select_states.py](./0-select_states.py)
+
+- This script lists all `states` from the database `hbtn_0e_0_usa`.
+- The SQL file for the database can be find here: [`hbtn_0e_0_usa`](./sql/0-select_states.sql).
+- Requirements:
+  - The script should take 3 arguments: `mysql username`, `mysql password` and
+    `database name` (no argument validation needed)
+  - The module MySQLdb (`import MySQLdb`) must be used
+  - The script should connect to a MySQL server running on `localhost` at port `3306`
+  - Results must be sorted in ascending order by states.id
+  - The code should not be executed when imported
